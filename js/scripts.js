d5f7d73e07cfb6a0f78e6007aa4edac7620acd41
john paul <jpaulopiy@gmail.com>
2021-06-21 11:06:34 +0300

    add new code from second trial, portfolio now disappears

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-body{
-    colour:red;
-}
+$(document).ready(function () {
+
+    $("#design").click(function () {
+        $("#desdesc").toggle();
+        $("#design").toggle();
+    });
+    $("#desdesc").click(function () {
+        $("#development").toggle();
+        $("#desdesc").toggle();
+    });
+    $("#development").click(function () {
+        $("#devp").toggle();
+        $("#development").toggle();
+    });
+    $("#devp").click(function () {
+        $("#development").toggle();
+        $("#devp").toggle();
+    });
+    $("#product").click(function () {
+        $("#prd").toggle();
+        $("#product").toggle();
+    });
+    $("#prd").click(function () {
+        $("#product").toggle();
+        $("#prd").toggle();
+    });
+});
