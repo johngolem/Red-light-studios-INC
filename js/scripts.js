a4eca280daae9c691153645f56adf1fc59ccfeec
john paul <jpaulopiy@gmail.com>
2021-06-23 05:59:34 +0300

    tried mailchimp, did not work for me

--- js/scripts.js
+++ js/scripts.js
@@ -20,66 +20,85 @@ $(document).ready(function(){
     });
   });
   
-  $(document).ready(function(){
-    $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
-    });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#overlay5").show();
-    }).mouseout(function(){
-      $("#overlay5").hide();
-    });
-    $("#work6").mouseover(function(){
-      $("#overlay6").show();
-    }).mouseout(function(){
-      $("#overlay6").hide();
-    });
-    $("#work7").mouseover(function(){
-      $("#overlay7").show();
-    }).mouseout(function(){
-      $("#overlay7").hide();
-    });
-    $("#work8").mouseover(function(){
-      $("#overlay8").show();
-    }).mouseout(function(){
-      $("#overlay8").hide();
+  $("#work4").mouseover(function () {
+    $("#work4").css('opacity', '0.5');
+    $("#dark1").show();
+    $("#dark1").addClass("projects");
+});
+$("#work4").mouseleave(function () {
+    $("#work4").css('opacity', '1');
+    $("#dark1").hide();
+});
+
+$(".work3").mouseover(function () {
+    $(".work3").css('opacity', '0.5');
+    $(".ontario").show();
+    $(".ontario").addClass("projects");
+});
+$(".work3").mouseleave(function () {
+    $(".work3").css('opacity', '1');
+    $(".ontario").hide();
+});
+
+$(".work2").mouseover(function () {
+    $(".work2").css('opacity', '0.5');
+    $(".polygon").show();
+    $(".polygon").addClass("projects");
+});
+$(".work2").mouseleave(function () {
+    $(".work2").css('opacity', '1');
+    $(".polygon").hide();
+});
+
+$(".work1").mouseover(function () {
+    $(".work1").css('opacity', '0.5');
+    $(".jim").show();
+    $(".jim").addClass("projects");
+});
+$(".work1").mouseleave(function () {
+    $(".work1").css('opacity', '1');
+    $(".jim").hide();
+});
+
+$(".work5").mouseover(function () {
+    $(".work5").css('opacity', '0.5');
+    $(".elena").show();
+    $(".elena").addClass("projects");
+});
+$(".work5").mouseleave(function () {
+    $(".work5").css('opacity', '1');
+    $(".elena").hide();
+});
+
+$(".work6").mouseover(function () {
+    $(".work6").css('opacity', '0.5');
+    $(".form").show();
+    $(".form").addClass("projects");
+});
+$(".work6").mouseleave(function () {
+    $(".work6").css('opacity', '1');
+    $(".form").hide();
+});
+
+$(".work7").mouseover(function () {
+    $(".work7").css('opacity', '0.5');
+    $(".burned").show();
+    $(".burned").addClass("projects");
+});
+$(".work7").mouseleave(function () {
+    $(".work7").css('opacity', '1');
+    $(".burned").hide();
+});
+
+$(".work8").mouseover(function () {
+    $(".work8").css('opacity', '0.5');
+    $(".giraffe").show();
+    $(".giraffe").addClass("projects");
+});
+$(".work8").mouseleave(function () {
+    $(".work8").css('opacity', '1');
+    $(".giraffe").hide();
+});
     });
     $("#sendButton").click(function () {
         var names = $("#names").val();
